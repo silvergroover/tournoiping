59c8621ad9bd3a5dd73354d8a791411bf55c7f64
silvergroover <mrouziou@gmail.com>
2019-03-24 21:53:19 +0100

    ajout d'un exemple de fichier local contenant les données de connexion à la base

---
+++ README.md
@@ -0,0 +1,2 @@
+# tournoiping
+create a local file in the include repository based on the local_sample.php file (copy it and replace the "mysomething" values by yours
---
+++ include/local_sample.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'mydbserveraddress');
+define('DB_NAME', 'mydbname');
+define('USER', 'mydbusername');
+define('PWD', 'mydbuserpassword');
+define('API_ID', 'my_fftt_API_ID');
+define('API_KEY', 'my_fftt_API_KEY');
+
+?>
